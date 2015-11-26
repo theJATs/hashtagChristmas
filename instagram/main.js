aea86c146163615d1a0f88e6a1c62e4c04867e65
Johan Wilhelmsson <rave@raves-mbp-3.client.rave.ac.uk>
2015-11-26 15:56:38 +0000

    target instagram tags

--- instagram/main.js
+++ instagram/main.js
@@ -4,23 +4,63 @@ var endpoint = "https://api.instagram.com/v1/tags/"
 var hashtag = "christmas"
 var client_id = "%20f5e6d7edf7cf4376a2aef55e9e761464"
 
-var URL = endpoint + hashtag + "/media/recent?client_id=" + client_id
+var instagramUrl = endpoint + hashtag + "/media/recent?client_id=" + client_id
 
 var hashtags = {} // we'll keep a count of hashtags
 
+var howManyInstagramCalls = 50
+var currentInstagramCallCounter = 0
 
 //var martinperssson = "https://api.instagram.com/v1/tags/martinperssson/media/recent?client_id=%20f5e6d7edf7cf4376a2aef55e9e761464";
 
-$.ajax(
+// RUN IT!
+
+getNextBatchOfPictures()
+
+// FUNCTIONS
+
+function getNextBatchOfPictures()
 {
-    dataType: "jsonp",
-    url: URL,
-    success: handleData
-})
+    if (currentInstagramCallCounter < howManyInstagramCalls)
+    {
+        console.log('calling Instagram ' + currentInstagramCallCounter)
+        getInstagramMediaByUrl(instagramUrl)
+        currentInstagramCallCounter += 1
+    }
+    else
+    {
+        console.log("we're done with Instagram, now let's analyse the data")
+        // remove all the tags that have less than X occurrences
+        
+        console.log(hashtags)
+        
+        // from http://stackoverflow.com/a/1069840
+        var commonTags = []
+        for (var tag in hashtags) commonTags.push( {tag:tag, count:hashtags[tag]} )
+        commonTags.sort(function(a, b) {return b.count - a.count})
+        
+//        console.log(commonTags)
+        
+        for (var i=1; i<5; i++)
+        {
+            console.log(commonTags[i].tag)
+        }
+    }
+}
+
+function getInstagramMediaByUrl(instagramUrl)
+{
+    $.ajax(
+    {
+        dataType: "jsonp",
+        url: instagramUrl,
+        success: handleData
+    })
+}
 
 function handleData( json )
 {
-//    console.log(json)
+    // console.log(json)
 
     var pictures = json.data
     
@@ -43,7 +83,7 @@ function handleData( json )
             {
                 if (_(blacklistedHashtags).contains(tag)) 
                 {
-                    console.log(tag + ' is blacklisted')
+                    // console.log(tag + ' is blacklisted')
                 }
                 else
                 {
@@ -55,18 +95,19 @@ function handleData( json )
 
         // console.log(picture)
 
-        var imageURL = picture.images.standard_resolution.url
+        /*var imageURL = picture.images.standard_resolution.url
         // console.log(imageURL)  
 
         var img = '<img src="' + imageURL + '">'
 
-        $('body').append(img)
+        $('body').append(img)*/
 
         counter ++
     }
     
+    //    console.log(hashtags)
     
-    console.log(hashtags)
-    
+    instagramUrl = json.pagination.next_url 
+    getNextBatchOfPictures()
 }
 
